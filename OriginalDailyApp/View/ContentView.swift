482803d9cc02706ca2aa2641e8e378cb692bed15
shu-0-lycorish <shu.kwn0257b@gmail.com>
2024-07-24 02:55:23 +0900

    Update to TabView placement with ForEach (contains incorrect behavior)

--- OriginalDailyApp/View/ContentView.swift
+++ OriginalDailyApp/View/ContentView.swift
@@ -9,36 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State var dailies: [DailyData] = Dailies
-    let maxMonth: Int = 3
-    @State var selectMonth: Int = 2
-    @State var prevMonth: Int = 1
-    @State var nextMonth: Int = 3
+    @State var filteredMonth = filterMonth(dailyData: Dailies)
+    @State var monthNum: Int = 1
+    //@State var selectMonth: Int = 2
+    //@State var prevMonth: Int = 1
+    //@State var nextMonth: Int = 3
     
-    var monthData = [Int]()
-    var correctMonth = 0
-    
-    var filteredMonth = filterMonth(dailyData: Dailies)
     
     
     var body: some View {
-        // prevMonth = selectMonth - 1
-        // nextMonth = selectMonth + 1
-        
         let _ = print(filteredMonth)
         
         VStack {
-            NavigationTitleView(selectMonth: $selectMonth)
-            
             TabView {
-                // ForEach(0..<maxMonth, id: \.self) { Data in
-                    // selectMonth = Int(Data)
-                    // DailyListView(dailies: $dailies, selectMonth: $selectMonth)
-                // }
+                ForEach(filteredMonth, id: \.self) {monthItem in
+                    VStack {
+                        let _ = monthNum = monthItem
+                        
+                        NavigationTitleView(selectMonth: $monthNum)
+                        DailyListView(dailies: $dailies, selectMonth: $monthNum)
+                    }
+                }
                 
-                
-                DailyListView(dailies: $dailies, selectMonth: $prevMonth)
-                DailyListView(dailies: $dailies, selectMonth: $selectMonth)
-                DailyListView(dailies: $dailies, selectMonth: $nextMonth)
+                //DailyListView(dailies: $dailies, selectMonth: $prevMonth)
+                //DailyListView(dailies: $dailies, selectMonth: $selectMonth)
+                //DailyListView(dailies: $dailies, selectMonth: $nextMonth)
             }
             .tabViewStyle(PageTabViewStyle())
             //.animation(.easeInOut)
